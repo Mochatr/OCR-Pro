9e749595ffd580d972178ae61080bd22d4c00cee
Mochatr <mohamedchatr8@gmail.com>
2024-11-20 16:11:55 +0100

    add main application structure with routing and Tailwind CSS integration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+
+function App() {
+    return (
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-50">
+        </div>
+      </BrowserRouter>
+    );
+  }
+  
+  export default App;
